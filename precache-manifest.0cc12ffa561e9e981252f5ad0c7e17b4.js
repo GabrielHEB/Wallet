9f8d3f71fe0ed303654533904686c873cb561d59
GabrielHEB <gabs.dev@hotmail.com>
2024-11-19 17:55:58 -0300

    Updates

---
+++ precache-manifest.0cc12ffa561e9e981252f5ad0c7e17b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2706e43e39e7b45c4ac8dc390c02bcc8",
+    "url": "/Wallet.github.io/index.html"
+  },
+  {
+    "revision": "cc0bd4fc120f0f3f5101",
+    "url": "/Wallet.github.io/static/css/main.7d13d112.chunk.css"
+  },
+  {
+    "revision": "ba3ffdee62a04b7234ef",
+    "url": "/Wallet.github.io/static/js/2.85e25898.chunk.js"
+  },
+  {
+    "revision": "3bb8f4be79104c3617df3eac6c3c9584",
+    "url": "/Wallet.github.io/static/js/2.85e25898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0bd4fc120f0f3f5101",
+    "url": "/Wallet.github.io/static/js/main.9d0dac6f.chunk.js"
+  },
+  {
+    "revision": "2f527a82d1b0ac807ff7",
+    "url": "/Wallet.github.io/static/js/runtime-main.1ac8f48f.js"
+  },
+  {
+    "revision": "395f42dc2b7420f6e85352c8fe7bc374",
+    "url": "/Wallet.github.io/static/media/wallet-attention.395f42dc.gif"
+  }
+]);
